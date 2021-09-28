a758a6f39432502d124fe1fa03d21cc663cf3030
Abayomi Aishida <yomicorperate@gmail.com>
2021-09-28 16:49:54 +0100

    Validate emails with validator and formatting errors to be more readable for users

--- models/User.js
+++ models/User.js
@@ -1,18 +1,29 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, "Please enter an email"],
     unique: true,
     lowercase: true,
+    validate: [isEmail, "Please enter a valid email"],
   },
   password: {
     type: String,
-    required: true,
-    minLength: 6,
+    required: [true, "Please enter a password"],
+    minlength: [6, "Mininum password length is 6 characters"],
   },
 });
+
+// fire a function after doc saved to db
+userSchema.post('save', function(doc, next) {
+  
+})
+
+userSchema.pre('save', function(next, this){
+
+})
 
 const User = mongoose.model("user", userSchema);
 
