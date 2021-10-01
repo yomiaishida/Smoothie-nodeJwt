ab22c3775483a871dda9b7190c0cca1c98b0bb53
Abayomi Aishida <yomicorperate@gmail.com>
2021-10-01 18:17:02 +0100

    Refactor: Connecting to DB with asynchronous funtion

--- app.js
+++ app.js
@@ -14,14 +14,24 @@ app.set("view engine", "ejs");
 // database connection
 const dbURI =
   "mongodb+srv://abayomi123:abayomi123@jwtcluster.mggjo.mongodb.net/jwt-icecream?";
-mongoose
-  .connect(dbURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(dbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("MongoDb Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+connectDb();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 // routes
 app.get("/", (req, res) => res.render("home"));
