9912c75021dd37f310b749d4508a024a75d7de0e
Abayomi Aishida <yomicorperate@gmail.com>
2021-10-01 18:33:34 +0100

    Hash passwords with bcrypt before they are save to Db

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -17,13 +18,12 @@ const userSchema = new mongoose.Schema({
 });
 
 // fire a function after doc saved to db
-userSchema.post('save', function(doc, next) {
-  
-})
 
-userSchema.pre('save', function(next, this){
-
-})
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 const User = mongoose.model("user", userSchema);
 
